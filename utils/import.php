1f0263091f1794160daf80e5d05012e767fcc4fb
Stuart Clark <stu@rtclark.net>
2014-07-13 08:18:25 +1000

    Added Species map to tree data.

--- utils/import.php
+++ utils/import.php
@@ -12,66 +12,97 @@ ini_set('memory_limit', '1024M');
 // @TODO - Settings in config file?
 $db = new \DB\SQL('mysql:host=localhost;port=3306;dbname=peeonatree', 'root', 'root');
 
-function import_trees($db) {
+function import_trees($db, $data = 'ballarat') {
   $entries = array();
 
-  $species_map = $db->exec('SELECT spid, scientific_name FROM species');
-  krumo($species_map);
-  exit;
+  $species_map = array();
+  $results     = $db->exec('SELECT spid, scientific_name FROM species');
+  foreach ($results as $result) {
+    $species_map[$result['scientific_name']] = $result['spid'];
+  }
 
   // Ballarat data.
-  //  $file = file_get_contents('../data/ballarat.json');
-  //  $data = json_decode($file);
-  //  foreach ($data->features as $tree) {
-  //    //  krumo($feature);exit;
-  //    $entries[] = array(
-  //      'tridx'     => $tree->properties->central_as,
-  //      'longitude' => $tree->geometry->coordinates[0],
-  //      'latitude'  => $tree->geometry->coordinates[1],
-  //      //    'species'   => $tree->properties->tspecies == 'Not Assessed' ? NULL : $tree->properties->tspecies,
-  //      'source'    => 'ballarat',
-  //    );
-  //  }
+  if ($data == 'ballarat') {
+    $file = file_get_contents('../../data/ballarat.json');
+    $data = json_decode($file);
+    foreach ($data->features as $tree) {
+      $species = $tree->properties->TSpecies == 'Not Assessed' ? NULL : strtolower($tree->properties->TSpecies);
+      $spid    = isset($species_map[$species]) ? $species_map[$species] : 0;
+
+      $entries[] = array(
+        'tridx'     => $tree->properties->central_as,
+        'longitude' => $tree->geometry->coordinates[0],
+        'latitude'  => $tree->geometry->coordinates[1],
+        'spid'      => $spid,
+        'source'    => 'ballarat',
+      );
+    }
+  }
 
   // Geelong
-  include('gPoint.php');
-  $file = file_get_contents('../data/geelong.geo.json');
-  $data = json_decode($file);
-  foreach ($data->features as $feature) {
-    // Convert GeoJSON co-ordinates to LatLong.
-    $gp = new gPoint();
-    $gp->setUTM($feature->geometry->coordinates[1], $feature->geometry->coordinates[0], '55H');
-    $gp->convertTMtoLL();
+  if ($data == 'geelong') {
+    include('gPoint.php');
+    $file = file_get_contents('../../data/geelong.geo.json');
+    $data = json_decode($file);
+    foreach ($data->features as $feature) {
+      $species = strtolower("{$feature->properties->genus} {$feature->properties->species}");
+      $spid    = isset($species_map[$species]) ? $species_map[$species] : 0;
 
-    $entries[] = array(
-      'tridx'     => $feature->properties->tree_id,
-      'longitude' => $gp->Long(),
-      'latitude'  => $gp->Lat(),
-      'age'       => $feature->properties->age,
-      'health'    => $feature->properties->health,
-      'source'    => 'geelong',
-    );
+      // Convert GeoJSON co-ordinates to LatLong.
+      $gp = new gPoint();
+      $gp->setUTM($feature->geometry->coordinates[1], $feature->geometry->coordinates[0], '55H');
+      $gp->convertTMtoLL();
+
+      $entries[] = array(
+        'tridx'     => $feature->properties->tree_id,
+        'longitude' => $gp->Long(),
+        'latitude'  => $gp->Lat(),
+        'spid'      => $spid,
+        'age'       => $feature->properties->age,
+        'health'    => $feature->properties->health,
+        'source'    => 'geelong',
+      );
+    }
   }
 
   // Melbourne data.
-  //    $file = file_get_contents('../data/melbourne.json');
-  //    $data = json_decode($file);
-  //    foreach ($data->data as $tree) {
-  //      $entries[] = array(
-  //        'tridx'     => $tree[8],
-  //        'longitude' => $tree[21],
-  //        'latitude'  => $tree[20],
-  //        'age'       => $tree[16],
-  //        //    'species'   => $tree->properties->tspecies == 'Not Assessed' ? NULL : $tree->properties->tspecies,
-  //        'source'    => 'melbourne',
-  //      );
-  //    }
+  if ($data == 'melbourne') {
+    $file = file_get_contents('../../data/melbourne.json');
+    $data = json_decode($file);
+    foreach ($data->data as $tree) {
+      $species = strtolower($tree[10]);
+      $spid    = isset($species_map[$species]) ? $species_map[$species] : 0;
+
+      $entries[] = array(
+        'tridx'     => $tree[8],
+        'longitude' => $tree[21],
+        'latitude'  => $tree[20],
+        'age'       => $tree[16],
+        'spid'      => $spid,
+        'source'    => 'melbourne',
+      );
+    }
+  }
 
   foreach ($entries as $entry) {
-    $db->insert('trees', $entry);
+    $values = $keys = array();
+    foreach ($entry as $key => $value) {
+      $keys[] = $key;
+      if (is_numeric($value)) {
+        $values[] = $value;
+      }
+      else {
+        $values[] = '"' . $value . '"';
+      }
+    }
+    $sql = 'INSERT INTO trees (' . implode(',', $keys) . ') VALUES (' . implode(', ', $values) . ')';
+    $db->exec($sql);
   }
 }
 
+/**
+ * @param $db
+ */
 function import_events($db) {
   $entries = array();
 
@@ -95,7 +126,7 @@ function import_events($db) {
   }
 }
 
-import_trees($db);
+import_trees($db, 'melbourne');
 //import_events($db);
 
 print 'success?';
